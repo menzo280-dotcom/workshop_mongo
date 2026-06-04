25607906fd0d0aa1d6697d6173e042220b8c7987
Enzo Martins <menzo280@gmail.com>
2026-06-03 21:34:18 -0300

    implement find user by id

---
+++ workshopmongo/src/main/java/com/enzo/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.enzo.workshopmongo.resources.exception;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.enzo.workshopmongo.service.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+		String error = "Object not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
--- workshopmongo/src/main/java/com/enzo/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/enzo/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.enzo.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enzo.workshopmongo.domain.User;
 import com.enzo.workshopmongo.repository.UserRepository;
+import com.enzo.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+	    Optional<User> obj = repo.findById(id);
+	    return obj.orElseThrow(() -> new ObjectNotFoundException("User not found"));
 	}
 
 }
