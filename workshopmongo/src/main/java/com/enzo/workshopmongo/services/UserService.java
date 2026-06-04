f0015a10df45668cf3cf31835906a7d3d39db73c
Enzo Martins <menzo280@gmail.com>
2026-06-04 09:28:41 -0300

    implement user update endpoint

--- workshopmongo/src/main/java/com/enzo/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/enzo/workshopmongo/services/UserService.java
@@ -15,28 +15,40 @@ import com.enzo.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
-	    Optional<User> obj = repo.findById(id);
-	    return obj.orElseThrow(() -> new ObjectNotFoundException("User not found"));
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("User not found"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.save(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = repo.findOne(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
