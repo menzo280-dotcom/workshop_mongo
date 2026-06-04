59742149064c36aa169866f5948a4fb40595ba83
Enzo Martins <menzo280@gmail.com>
2026-06-04 11:18:44 -0300

    implement simple query with query methods

--- workshopmongo/src/main/java/com/enzo/workshopmongo/resources/PostResource.java
+++ workshopmongo/src/main/java/com/enzo/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.enzo.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.enzo.workshopmongo.domain.Post;
+import com.enzo.workshopmongo.resources.util.URL;
 import com.enzo.workshopmongo.services.PostService;
 
 @RestController
@@ -17,11 +21,20 @@ public class PostResource {
 	@Autowired
 	private PostService service;
 
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(
+	        @RequestParam(value = "text", defaultValue = "") String text) {
+	    text = URL.decodeParam(text);
+	    List<Post> list = service.findByTitle(text);
+	    return ResponseEntity.ok().body(list);
+	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	
 
 }
